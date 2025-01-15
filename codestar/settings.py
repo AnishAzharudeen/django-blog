8d2a43b71aa05c8f2d1e7d676e816c339b7c6559
Anish Fatima <fatima.anish@gmail.com>
2025-01-15 21:15:50 +0000

    Add database creation changes

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-04eejq(e)3a&#@!4p_5@ja#g2=l$#fl^+htj2%z1z0x9vm4kap'
-
+#SECRET_KEY = 'django-insecure-04eejq(e)3a&#@!4p_5@ja#g2=l$#fl^+htj2%z1z0x9vm4kap'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -88,6 +88,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
